c9482c11ed15f6ac59f76fd7f2925baa6f79fc44
AdamW <adam.wolf.personal@gmail.com>
2025-04-19 19:50:10 +0200

    add more content for "Coming soon"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import machine from './assets/machine.png';
+import favico from '../favico.png';
+
+
 const App = () => {
   return (
     <div className="homepage">
+      <div className='logo'>
+        <img className="machine" src={machine} alt="Machine" />
+        <img className="favico rotating" src={favico} alt="Machine" />
+      </div>
       <h1>Hamarosan jövünk</h1>
     </div>
   )
 }
 
-export default App
+export default App;
