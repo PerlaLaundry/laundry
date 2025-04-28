8230a140a63183ed0edc06e469808eb022e45806
AdamW <adam.wolf.personal@gmail.com>
2025-04-28 21:20:54 +0200

    add routing to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useEffect } from "react";
-import { useTranslation } from "react-i18next";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import machine from "./assets/machine.png";
-import brand from "./assets/brand.svg";
-import favico from "../favico.png";
-import i18n from "./utils/i18n";
+import Home from "./pages/home/Home";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
-  const { t } = useTranslation();
-
-  // Dev code to test the language switcher extention
-  useEffect(() => {
-    if (import.meta.env.VITE_API_ENV === "development") {
-      const lang = localStorage.getItem("LOCALE_SWITCHER_LANGUAGE");
-
-      i18n.changeLanguage(lang);
-    }
-    // eslint-disable-next-line
-  }, [localStorage]);
-
   return (
-    <div className="homepage">
-      <div className="brand">
-        <img src={brand} />
-      </div>
-      <div className="logo">
-        <img className="machine" src={machine} alt="Machine" />
-        <img className="favico rotating" src={favico} alt="Machine" />
-      </div>
-      <h1>{t("openingSoon")}</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="gyakran-ismetelt-kerdesek"
+            element={<div className="layout">GYIK</div>}
+          />
+          <Route
+            path="szabalyzat"
+            element={<div className="layout">Szabályzat</div>}
+          />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
