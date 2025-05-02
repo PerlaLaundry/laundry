0554d722680ef52dcc75aaa07ea7a05cd10cc644
Adam Wolf <38081038+wolf-adam@users.noreply.github.com>
2025-05-03 01:52:54 +0200

    Refactor/add mobile view (#3)
    * minor i18n content update
    
    * add mobile view (tested on Pixel 7, iphone XR, 12 Pro, 14 Pro Max)

--- src/pages/home/Location.jsx
+++ src/pages/home/Location.jsx
@@ -68,11 +68,14 @@ const Location = () => {
           </div>
         </div>
       </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2767.8302474383745!2d18.219516576855103!3d46.074425871090554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4742b1000393e3f7%3A0x988f517b878667bc!2sPerla%20Laundry!5e0!3m2!1shu!2shu!4v1746189056403!5m2!1shu!2shu"
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <div className="location-iframe">
+        <iframe
+          width="100%"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2767.8302474383745!2d18.219516576855103!3d46.074425871090554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4742b1000393e3f7%3A0x988f517b878667bc!2sPerla%20Laundry!5e0!3m2!1shu!2shu!4v1746189056403!5m2!1shu!2shu"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </div>
     </section>
   );
 };
