007055d36cf9372f9148d6774a593cf8a6e3d8b4
AdamW <adam.wolf.personal@gmail.com>
2025-05-17 22:49:09 +0200

    add new werk video section to homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import Introduction from "./Introduction";
 import Services from "./Services";
@@ -8,6 +9,7 @@ import AboutUs from "./AboutUs";
 import Location from "./Location";
 
 const Home = () => {
+  const { t } = useTranslation();
   const location = useLocation();
 
   // This is used to scroll to the section when the user clicks on the navigation
@@ -23,6 +25,17 @@ const Home = () => {
     <div className="homepage">
       <Introduction />
       <Services />
+      <div className="intro-video">
+        <iframe
+          src="https://www.youtube.com/embed/y6120QOlsfU?si=pxHLOdwk2rqUn9oA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        />
+        <h2>{t("introduction.video")}</h2>
+      </div>
       <HowToUse />
       <AboutUs />
       <Location />
